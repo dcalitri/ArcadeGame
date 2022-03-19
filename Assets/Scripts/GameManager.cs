b3e06cffa992403e3a9eaa22f55410b2a851801c
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-03-19 15:02:04 -0400

    Minor change to UI Script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject titleScreen;
+    private Button button;
+
 
     // Start is called before the first frame update
     void Start()
