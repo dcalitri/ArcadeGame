0d0dba030807cec4b970c63dd13c5228f0a57fc6
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-03-21 16:08:44 -0400

    Redid things

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 5.0f;
+    public float speed = 10.0f;
     public float jumpForce;
     public float gravityModifier;
     public float powerupStrength = 2.0f;
@@ -17,8 +17,6 @@ public class PlayerController : MonoBehaviour
     private GameManager gameManager;
     private float yBound = -5;
     private float xRange = 9;
-    private float boostTimer;
-    private bool boosting;
 
     // Start is called before the first frame update
     void Start()
@@ -26,20 +24,12 @@ public class PlayerController : MonoBehaviour
         player2Rb2d = GetComponent <Rigidbody2D>();
         Physics.gravity *= gravityModifier;
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-<<<<<<< Updated upstream
         
-=======
-
->>>>>>> Stashed changes
         // How to Quit back to the Arcade Machine main menu
         if (Input.GetKeyUp(KeyCode.Escape))
         {
             Application.Quit();
         }
-
-        speed = 5;
-        boostTimer = 0;
-        boosting = false;
     }
 
     // Update is called once per frame
@@ -85,18 +75,6 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
             gameManager.GameOver();
         }
-
-        if (boosting)
-        {
-            boostTimer += Time.deltaTime;
-
-            if (boostTimer >= 3)
-            {
-                speed = 5;
-                boostTimer = 0;
-                boosting = false;
-            }
-        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision2D)
@@ -118,7 +96,7 @@ public class PlayerController : MonoBehaviour
             Vector3 awayFromPlayer = (collision2D.gameObject.transform.position - transform.position);
 
             player2RigidBody2D.AddForce(awayFromPlayer * powerupSpeedBoost, ForceMode2D.Impulse);
-            Debug.Log("Collided with " + collision2D.gameObject.name + " with powerup set to " + hasPowerup);
+            Debug.Log("Collided with " + collision2D.gameObject.name + " with powerup set to " + hasPowerup2);
         }
     }
 
@@ -137,18 +115,11 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
-
-        if (other.CompareTag("Powerup2"))
-        {
-            boosting = true;
-            speed = 12.0f;
-            Destroy(other.gameObject);
-        }
     }
 
     IEnumerator PowerupCountdownRoutine()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(5);
         hasPowerup = false;
     }
 }
