81b2e4a6b2b629e0f64962afe450c65a2dc647d6
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-03-21 13:29:36 -0400

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,17 +7,31 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] powerupPrefabs;
     public int powerupIndex;
     public float spawnRangeX = 5.0f;
+    private GameManager gameManager;
+    private float spawnStart = 1.0f;
+    private float spawnDelay = 1.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         int powerupIndex = Random.Range(0, powerupPrefabs.Length);
-        Instantiate(powerupPrefabs[powerupIndex], new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 5), powerupPrefabs[powerupIndex].transform.rotation);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        
+        if (gameManager.isGameActive)
+        {
+            InvokeRepeating("SpawnPowerUps", spawnStart, spawnDelay);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnPowerUps()
+    {
+        Instantiate(powerupPrefabs[powerupIndex], new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 5), powerupPrefabs[powerupIndex].transform.rotation);
     }
 }
