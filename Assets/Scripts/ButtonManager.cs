b299f0ab31791e52c0938d9f5e395110a9f17dab
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-03-25 09:11:04 -0400

    Started ButtonManager Script

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject howToPlayMenu;
+    public GameObject gameOverScreen;
+    public Button howToPlayButton;
+    public Button backButton;
+    public Button restartButton;
+
+
     // Start is called before the first frame update
-    void Start()
+    void HowToPlayButtonPressed()
     {
         
     }
